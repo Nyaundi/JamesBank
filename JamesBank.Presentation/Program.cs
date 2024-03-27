94d8b50d47d3fcf2a2557fc36dfb7ddcdef840dd
Nyaundi <jamesmabuka260@gmail.com>
2024-03-27 19:04:52 +0300

    Display Title of the app

--- JamesBank.Presentation/Program.cs
+++ JamesBank.Presentation/Program.cs
@@ -1,15 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JamesBank.Presentation
+﻿class Program
 {
-    internal class Program
+    //Application execution starts from the main method
+    static void Main()
     {
-        static void Main(string[] args)
+        //Display Title of the app
+        System.Console.WriteLine("****************James Bank * ***************");
+        System.Console.WriteLine("::Login Page");
+
+        //declaring variables to store username and password;
+        string userName = null, password = null;
+
+        //read userName from Keyboard
+        System.Console.Write("Username: ");
+        userName = System.Console.ReadLine();
+
+        //read password from keyboard only if username is entered
+        if (userName != "")
         {
+            //read password from keyboad
+            System.Console.Write("Password: ");
+            password = System.Console.ReadLine();
         }
+
+        //Validating the data i.e check username and password
+        if(userName =="system" && password == "manager")
+        {
+            System.Console.WriteLine("To DO: Main Menu");
+        }
+
+        System.Console.WriteLine("Thank you! Visit Again");
+        System.Console.ReadKey();
+
     }
 }
